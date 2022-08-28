1fe966386811d35dd3975a596ddc33f9288c4bba
mdasif8393 <md.asif.8393@gmail.com>
2022-08-28 17:24:04 +0600

    set local storage add item

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { addToDb } from '../../utilities/fakedb';
 
 const Cosmetic = (props) => {
-    const {name, price} = props.cosmetic;
+    const {name, price, _id} = props.cosmetic;
+    
     return (
         <div>
             <h2>Buy this: {name}</h2>
             <p>Only for $: {price}</p>
+            <small>it has id: {_id}</small> <br />
+            <button onClick={()=>addToDb(_id)}>add to cart</button>
         </div>
     );
 };
--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,19 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cosmetic from '../Cosmetic/Cosmetic';
 import './Cosmetics.css'
 const Cosmetics = () => {
-    const cosmetics = [
-        {id:1, name:'Alta', price:100},
-        {id:2, name:'Palish', price:200},
-        {id:3, name:'Malish', price:300},
-        {id:4, name:'Balish', price:400},
-        {id:5, name:'Nalish', price:500},
-    ]
+
+    const [cosmetics, setCosmetics] = useState([]);
+    fetch('data.json')
+    .then(res => res.json())
+    .then(data => setCosmetics(data))
     return (
         <div>
             <h1>Welcome to my cosmetics store</h1>
             {
-                cosmetics.map(cosmetic => <Cosmetic key={cosmetic.id} cosmetic={cosmetic}></Cosmetic>)
+                cosmetics.map(cosmetic => <Cosmetic key={cosmetic._id} cosmetic={cosmetic}></Cosmetic>)
             }
         </div>
     );
